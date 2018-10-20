e685d33d060c8bbca387c62fac44de3848bc25a0
SavageSides <42398425+SavageSides@users.noreply.github.com>
2018-10-20 09:34:41 -0500

    Create meme.py

---
+++ meme.py
@@ -0,0 +1,20 @@
+import discord
+import aiohttp
+import random
+from discord.ext import commands 
+
+@client.command(pass_context = True)
+async def meme(ctx):
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get("https://api.reddit.com/r/me_irl/random") as r:
+            author = ctx.message.author
+            data = await r.json()
+            embed = discord.Embed(title="Meme:",
+
+                                  color=0x00ff00)
+            embed.set_image(url = data[0]["data"]["children"][0]["data"]["url"])
+            embed.set_footer(icon_url=author.avatar_url, text="| Fun Commands!")
+
+            await client.say(embed=embed)
+
+#You need to get this code and import the things below!
